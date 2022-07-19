7f9dfff05b88afc802f82c453bb980abf80291bf
Jan Czok <jan.czok@outlook.de>
2022-07-19 10:17:39 +0200

    added dice

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ document.querySelector("#konto").innerHTML = konto + "€";
 
 function run(){
     const random = Math.round(Math.random() * 5 + 1);
-    document.querySelector("#zahl").innerHTML = random;
+    switch(random){
+        case 1: document.querySelector(".fa-solid").classList = "fa-solid fa-dice-one"; break;
+        case 2: document.querySelector(".fa-solid").classList = "fa-solid fa-dice-two"; break;
+        case 3: document.querySelector(".fa-solid").classList = "fa-solid fa-dice-three"; break;
+        case 4: document.querySelector(".fa-solid").classList = "fa-solid fa-dice-four"; break;
+        case 5: document.querySelector(".fa-solid").classList = "fa-solid fa-dice-five"; break;
+        case 6: document.querySelector(".fa-solid").classList = "fa-solid fa-dice-six"; break;
+    }
     if(random == 6){
         konto+=5;
     } else {
